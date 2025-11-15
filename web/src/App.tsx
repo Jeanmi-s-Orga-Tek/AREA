2f6359ae224df0d709da55f33b4ae83bb020a143
Ligsow <edouarddupuch.ed@gmail.com>
2025-11-15 19:34:54 +0100

    [ADD] initialize Vite React client

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** AREA
+** File description:
+** App
+*/
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "1rem" }}>
+      <h1>AREA Web Client</h1>
+      <p>React + Vite + TypeScript scaffold is ready.</p>
+    </div>
+  );
+}
+
+export default App;
